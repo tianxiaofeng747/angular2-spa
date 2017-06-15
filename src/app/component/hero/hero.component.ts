78c9921939ab53d11a8eb72522b5e756bc5b5cd7
smallKugua <tianxiaofeng747@163.com>
2017-06-15 13:31:03 +0800

    update

--- src/app/component/hero/hero.component.ts
+++ src/app/component/hero/hero.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
     selector: 'hero',
+    styleUrls: ['./hero.component.less'],
     templateUrl: './hero.component.html'
 })
 export class HeroComponent {
@@ -10,15 +11,15 @@ export class HeroComponent {
 
 
 
-    heroList:Array<Object> = [{
-        id:1,
-        name:'侯亮平'
-    },{
-        id:2,
-        name:'李达康'
-    },{
-        id:3,
-        name:'祈同伟'
+    heroList: Array<Object> = [{
+        id: 1,
+        name: '侯亮平'
+    }, {
+        id: 2,
+        name: '李达康'
+    }, {
+        id: 3,
+        name: '祈同伟'
     }];
     delete = function (obj) {
         let id = obj.id;
@@ -36,6 +37,42 @@ export class HeroComponent {
     //跳转路由
     gotoUrl = () => {
         console.log('exec')
-        this.router.navigate(['../list'], {relativeTo: this.route});
+        this.router.navigate(['../list'], { relativeTo: this.route });
+    };
+
+    myStyle: any = {
+        marginLeft: 0
+    };
+    bannerList: Array<Object> = [
+        {
+            src: '../assets/images/1.jpg'
+        },
+        {
+            src: '../assets/images/2.jpg'
+        },
+        {
+            src: '../assets/images/3.jpg'
+        },
+        {
+            src: '../assets/images/4.jpg'
+        }
+    ]
+    ngAfterViewInit() {
+        console.log(this)
+        let containWidth = document.getElementById('slide_contain').offsetWidth,
+            current = 0,
+            self = this, move = () => {
+                current++;
+                if (current < self.bannerList.length) {
+                    self.myStyle.marginLeft = -containWidth * current + 'px';
+                }
+                else {
+                    self.myStyle.marginLeft = 0;
+                    current = 0;
+                }
+                setTimeout(move, 4000);
+            };        
+        self.myStyle.width = self.bannerList.length * 100 + '%';
+        setTimeout(move, 4000)
     }
 }
